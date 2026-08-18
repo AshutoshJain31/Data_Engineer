f74ffe5434357d4523f8406b8c29fd2b1678ff08
Jain <Ashutosh.Jain@saint-gobain.com>
2026-08-18 13:18:32 +0530

    First Commit

---
+++ day1/Employee_Info.py
@@ -0,0 +1,26 @@
+name="Ashuosh Jain"
+age=27
+current_role="Software Engineer"
+experience=5
+expect_salary=123.23
+Target_role="Data Engineer"
+python_learning_status=True
+
+
+# print("=============================")
+# print("EMPLOYEE INFORMATION")
+# print("=============================")
+# print("Name:",name)
+# print("Age:",age)  
+# print("Current Role:",current_role)
+# print("Experience:",experience,"years")
+# print("Expected Salary:",expect_salary)
+# print("Target Role:",Target_role)
+# print("Python Learning Status:",python_learning_status)
+
+print(f"My name is {name} and I am {age} years old.I have {experience} years of experience in software development. My current role is {current_role} and my target role is {Target_role}. I am expecting a salary of {expect_salary} and my python learning status is {python_learning_status}.")
+
+current_salary=10000
+target_salary=30000
+
+print(f"difference between current and expected salary is {target_salary-current_salary}")
---
+++ day1/day1.py
@@ -0,0 +1,89 @@
+# print("My name is ashutosh jain")
+# print("I have 5+ years of experience in software development.")
+# print("My goal is to become the data engineer")
+
+
+name="Ashutosh Jain"
+age=27
+role="Software Engineer"
+experience=5
+salary=123.23
+boolean_value=True
+Phone_number = 9898989898
+
+# print("My name is", name , "and I am",age,"years old.")
+# print("I have", experience, "years of experience in software development.") 
+# print("My role is",role)
+# print("My goal is to become a data engineer.")
+
+# print(type(name))
+# print(type(age))
+# print(type(role))
+# print(type(salary))
+# print(type(boolean_value))
+# print(type(Phone_number))
+
+# Task 1
+number1=20
+number2=30
+
+sum = number1 + number2;
+
+# print(sum)
+# print("The sum of ",number1,"and",number2,"is",sum)
+# print("type of sum is",type(sum))
+
+
+# Task 2
+
+first_name="Ashutosh"
+last_name="jain"
+experience=5
+print(f"My name is {first_name} {last_name} and I have total of {experience} years of experience in software development.")
+print("i have",str(experience),"years of experience in software development.")
+print("my name is",first_name + " "+ last_name)
+print(first_name +" " + last_name)
+
+# Task 3
+value1=10
+value2="20"
+
+print(type(value1))
+print(type(value2))
+print(value1+int(value2))
+
+
+#taks 4
+
+current_salary=10000
+target_salary=20000
+
+print("My expected salary increment is ",target_salary-current_salary)
+
+
+# Task 5
+
+current_age="27"
+current_salary="10000"
+
+print(type(current_age))
+print(type(current_salary))
+
+current_age_int=int(current_age)
+current_salary_int=int(current_salary)
+
+print(type(current_age_int))
+print(type(current_salary_int))
+
+# Task 6
+
+current_salary="100.123"
+
+print(type(current_salary))
+
+curent_salary=float(current_salary)
+
+print(type(curent_salary))
+
+print("My current salary is",curent_salary+1000)    
+
